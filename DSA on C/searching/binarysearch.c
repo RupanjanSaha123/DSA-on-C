88d6646d9c53de6562aa69c5d8a9681e111300f7
Rupanjan Saha <rupanjan1203@gmail.com>
2025-08-21 13:28:15 +0530

    Add files via upload

---
+++ DSA on C/searching/binarysearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, key, low, high, mid, found = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            found = mid;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (found != -1)
+        printf("Element found at index %d\n", found);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
